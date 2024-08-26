7bb68a0ca4016b8a1c459bf04481b08f493172cf
henriquetomazzz <henriquetomaz021@gmail.com>
2024-08-25 23:05:11 -0300

    style: ajusta formatação

--- beecrowd/lista-05-2024/1174.py
+++ beecrowd/lista-05-2024/1174.py
@@ -4,8 +4,8 @@ def vetor():
   num = [float(input()) for _ in range(100)]
   
   for i in range(100):
-        if num[i] <= 10:
-            print(f"A[{i}] = {num[i]:.1f}")
+    if num[i] <= 10:
+      print(f"A[{i}] = {num[i]:.1f}")
 
 
 vetor()
