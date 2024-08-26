117d0d7fc6d4330a0210e210c1427ab422033bb2
henriquetomazzz <henriquetomaz021@gmail.com>
2024-08-26 19:04:46 -0300

    resolve questões ex102 e ex103

--- cursoemvideo/mundo-3/ex102.py
+++ cursoemvideo/mundo-3/ex102.py
@@ -1,3 +1,16 @@
 '''crie um porgrama que tenha uma função fatorial() que receba dois parametros: o primeiro que indique um 
 número a calcular e o outro chamado show, que será o valor lógico(opcional) indicando se será mostrado ou 
 não na tela o processo de cálculo do fatorial'''
+def fatorial(num, show=False):
+  lista = []
+  if show == False: 
+    if num == 1: 
+      return 1
+    return num * fatorial(num-1)
+  
+  elif show == True: 
+    if num == 1: 
+      return 1
+    print(fatorial(num-1) + "X" + fatorial(num-2) "="* num)
+
+print(fatorial(5, show=True))
--- cursoemvideo/mundo-3/ex103.py
+++ cursoemvideo/mundo-3/ex103.py
@@ -1,3 +1,14 @@
 '''faça um programa que tenha uma função chamado ficha(), que receba dois paramêtros opcionais: o nome de um
 jogador e quantos gols ele marcou. O prorama deverá ser capaz de mostrar a ficha do jogador, mesmo que algum
 dado não tenha sido informado corretamente.'''
+def ficha(jogador = '<desconhecido>', gol = 0):
+  if jogador == '' and gol == '': 
+    print(f"O jogador {jogador} fez {gol} gol(s) no campeonato")
+  else: 
+    print(f"O jogador {jogador} fez {gol} gol(s) no campeonato")
+
+print("-"*30)
+jogador = input("Nome do Jogador: ")
+gol = input("Número de gols: ")
+
+ficha(jogador, gol)
