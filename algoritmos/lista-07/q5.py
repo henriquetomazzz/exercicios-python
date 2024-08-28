f50ee4040e5a0a188f4ad58d2f0d6989ada6ae59
henriquetomazzz <henriquetomaz021@gmail.com>
2024-08-28 11:01:15 -0300

    adiciona join

--- algoritmos/lista-07/q5.py
+++ algoritmos/lista-07/q5.py
@@ -1,12 +1,12 @@
 '''Escreva um algoritmo que leia 10 nomes e os escreva em ordem alfabética.'''
 def ordem():
   lista.sort()
-  print(f"A lista de nomes ordenados ficara {lista}")
+  print(f"A lista de nomes ordenados ficara {", ".join(map(str, lista))}")
 
 lista = []
 
 def entrada():
-  for i in range(0, 10):
+  for i in range(0, 2):
     nome = input("Escreva o nome: ")
     lista.append(nome)
   ordem()
