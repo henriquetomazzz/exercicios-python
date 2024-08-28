fa0ba061e3148e243ebfcc5c025dbcc902c2aba2
henriquetomazzz <henriquetomaz021@gmail.com>
2024-08-28 13:36:57 -0300

    resolve questão 06 da lista 07

--- algoritmos/lista-07/q6.py
+++ algoritmos/lista-07/q6.py
@@ -1,3 +1,14 @@
 '''Escreva um algoritmo que leia 10 numeros e os escreva em ordem decrescente.'''
-def numeros(): 
+def decresente(): 
+  lista.sort(reverse=True)
+  print(f"A lista ficará {" ".join(map(str, lista))}")
 
+lista = []
+
+def entrada():
+  for i in range(0,10):
+    num = int(input())
+    lista.append(num)
+  decresente()
+
+entrada()
