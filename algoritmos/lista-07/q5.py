2bd310d625d4527723413c1ec3bd238a953fd292
henriquetomazzz <henriquetomaz021@gmail.com>
2024-08-28 10:23:35 -0300

    resolve questão 05 da lista 07

---
+++ algoritmos/lista-07/q5.py
@@ -0,0 +1,14 @@
+'''Escreva um algoritmo que leia 10 nomes e os escreva em ordem alfabética.'''
+def ordem():
+  lista.sort()
+  print(f"A lista de nomes ordenados ficara {lista}")
+
+lista = []
+
+def entrada():
+  for i in range(0, 4): 
+    nome = input("Escreva o nome: ")
+    lista.append(nome)
+  ordem()
+
+entrada()
