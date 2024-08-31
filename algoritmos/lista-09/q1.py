a09efe99694175639bf979855fef45b004ea85c4
henriquetomazzz <henriquetomaz021@gmail.com>
2024-08-30 23:50:21 -0300

    resolve questão 01 da lista 09

--- algoritmos/lista-09/q1.py
+++ algoritmos/lista-09/q1.py
@@ -1,3 +1,28 @@
 '''Tuconaldo Tripa é mecânico e anda meio broco. Precisa de sua ajuda. Desenvolva um programa que receba os 
 nomes das peças que ele está removendo,  na ordem, e depois vá indicando, uma a uma,  na ordem inversa da 
 adição, as peças para remontar o carro. Operações adicionar peça e remover peça.'''
+lista = []
+def adicionar():
+  
+  lista.reverse()
+  print("Agora vamos fazer o processo contrário\n")
+  print(f"A ordem será {", ".join(map(str, lista))}\n")
+
+def remover():
+  peca = input("Qual peça você deseja trocar: ") 
+
+  while True: 
+    troca = input("Digite o nome da peça removida: ")
+    if peca != troca: 
+      lista.append(troca)
+      continue
+    else: 
+      lista.append(peca)
+      break
+  print("="*50)
+  print(f"As peças removidas foram {", ".join(map(str, lista))}")
+  print("="*50)
+
+  adicionar()
+
+remover()
