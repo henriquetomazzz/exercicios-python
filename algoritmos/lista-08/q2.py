ff765a633c2fef067376960cf2f41b04e6c0d266
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-01 16:42:09 -0300

    resolve questão 02 da lista 08

--- algoritmos/lista-08/q2.py
+++ algoritmos/lista-08/q2.py
@@ -1,2 +1,24 @@
 '''Escreva um algoritmo que leia um inteiro  n e uma matriz de dimensão escolhida pelo usuário. Em seguida, 
 deve calcular e escrever o resultado do produto escalar.'''
+def produtoEscalar(n, matriz):
+    resultante = []
+    for linha in matriz:
+        linhaR = []
+        for e in linha: 
+            linhaR.append(n*e)
+        resultante.append(linhaR)
+
+    return resultante
+
+linhas,colunas = map(int,input("Digite as dimensões da matriz: ").split("x"))
+
+matriz = []
+for i in range(linhas):
+    matriz.append(list(map(int,input("Digite uma das linhas: ").split(" "))))
+
+n = int(input("Escalar: "))
+
+resultado = produtoEscalar(n, matriz)
+
+for linha in resultado:
+    print(" ".join(map(str,linha)))
