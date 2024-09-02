e2ea94a8112f85a0d40c8f6aa177b2b61a485627
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-02 11:26:53 -0300

    adiciona enunciado das questões da lista 06

--- beecrowd/lista-06-2024/1180.py
+++ beecrowd/lista-06-2024/1180.py
@@ -1,13 +1,15 @@
-lista = []
+'''Faça um programa que leia um valor N. Este N será o tamanho de um vetor X[N]. A seguir, leia cada um dos 
+valores de X, encontre o menor elemento deste vetor e a sua posição dentro do vetor, mostrando esta informação.
 
-def entrada():
-  rep = int(input())
+Entrada
+A primeira linha de entrada contem um único inteiro N (1 < N < 1000), indicando o número de elementos que
+deverão ser lidos em seguida para o vetor X[N] de inteiros. A segunda linha contém cada um dos N valores, 
+separados por um espaço. Vale lembrar que nenhuma entrada haverá números repetidos.
 
-  var = list(map(int, input().split()))
-  lista.append(var)
-#  for i in range(rep): 
-#    var = list(map(int, input().split()))
-#    lista.append(var)
-#  print(min(lista))
+Saída
+A primeira linha apresenta a mensagem “Menor valor:” seguida de um espaço e do menor valor lido na entrada. 
+A segunda linha apresenta a mensagem “Posicao:” seguido de um espaço e da posição do vetor na qual se encontra
+o menor valor lido, lembrando que o vetor inicia na posição zero.'''
 
-entrada()
+entrada = int(input())
+
--- beecrowd/lista-06-2024/1185.py
+++ beecrowd/lista-06-2024/1185.py
@@ -1,6 +1,6 @@
 '''Leia um caractere maiúsculo, que indica uma operação que deve ser realizada e uma matriz M[12][12]. 
 Em seguida, calcule e mostre a soma ou a média considerando somente aqueles elementos que estão acima da 
-diagonal principal da matriz, conforme ilustrado abaixo (área verde).
+diagonal secundária da matriz, conforme ilustrado abaixo (área verde).
 
 Entrada
 A primeira linha de entrada contem um único caractere Maiúsculo O ('S' ou 'M'), indicando a operação (Soma ou 
@@ -10,27 +10,3 @@ compõem a matriz.
 Saída
 Imprima o resultado solicitado (a soma ou média), com 1 casa após o ponto decimal.
 '''
-def entrada():
-  lista = []
-  letra = input()
-  contador = 0
-
-  if letra == 'S':
-    for i in range(0, 144):
-      o = float(input())
-      if o > 0:
-        lista.append(o)
-        total = sum(lista)
-    print(f"{total:.1}")
-
-  if letra == 'M':
-    for j in range(0, 144):
-      o = float(input())
-      contador += 1
-      if o > 0:
-        lista.append(o)
-        total = sum(lista)
-
-    print(f"{total/contador:.1f}")
-    
-entrada()
