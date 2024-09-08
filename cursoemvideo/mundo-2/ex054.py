358524ddbadedc5f8921f11db8147bee0f7f1e49
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-08 15:39:55 -0300

    resolve ex054 do mundo 2

--- cursoemvideo/mundo-2/ex054.py
+++ cursoemvideo/mundo-2/ex054.py
@@ -1,9 +1,12 @@
 '''Crie um programa que leia o ano de nascimento de sete pessoas. No final mostre qauntas pessoas ainda não atingiram
 a maioridade e quantos já são de maiores.'''
+from datetime import date
+
 for c in range(1, 8):
-  idade = int(input("Digite a sua idade: "))
+  ano = int(input("Digite o ano em que você nasceu: "))
+  data = date.today().year
   
-  if idade < 18:
-    print("Você não atingiou a maioridade!")
-  elif idade >= 18:
+  if data-ano < 18:
+    print("Você ainda não atingou a maioridade!")
+  elif data-ano >= 18:
     print("Você já atingiu a maioridade!")
