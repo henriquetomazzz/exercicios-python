07eba6f74348f242a33ab91bc05d69357f23c551
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-14 20:04:13 -0300

    resolve ex049 do mundo 2

--- cursoemvideo/mundo-2/ex049.py
+++ cursoemvideo/mundo-2/ex049.py
@@ -1,6 +1,6 @@
 '''Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando o laço
 for'''
-entrada = float(input("Digite um número: "))
+entrada = float(input("Digite um número para ver sua tabuada: "))
 print("TABUADA")
 for c in range(0, 11, 1):
-  print(f"{c} X {entrada} = {c*entrada}")
+  print(f"{entrada} X {c} = {c*entrada}")
