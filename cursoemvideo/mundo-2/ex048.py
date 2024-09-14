92d742813f50e42e3fe812fa04d94c714243bf8f
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-14 20:01:27 -0300

    resolve ex048 do mundo 2

--- cursoemvideo/mundo-2/ex048.py
+++ cursoemvideo/mundo-2/ex048.py
@@ -1,6 +1,10 @@
 '''Faça um programa que calcule a soma entre todos os números ímpares que são multiplos de 3 e que se encontram no
 intervalo de 1 até 500.'''
-for c in range(1, 501):
-  if c%2 == 1:
-    if c%3 == 0:
-      print(c)
+soma = 0
+cont = 0
+for c in range(1, 501, 2):
+  if c%3 == 0:
+    soma += c
+    cont += 1
+
+print(f"A soma de todos os {cont} valores solicitados é {soma}")
