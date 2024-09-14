844d0ae00eceeb08a89094ce78d07a01e1c4ecfc
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-14 20:15:16 -0300

    resolve ex051 do mundo 2

--- cursoemvideo/mundo-2/ex051.py
+++ cursoemvideo/mundo-2/ex051.py
@@ -1,2 +1,8 @@
 '''Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final mostre os 10 primeiros termos dessa
 progressão'''
+termo = int(input("Primeiro termo: "))
+razao = int(input("Razão: "))
+
+for i in range(termo, razao*11, razao):
+  print(f"{i}", end='-> ')
+print("ACABOU")
