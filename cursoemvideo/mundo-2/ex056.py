de20388eeabae8fcf0310e379585a6633b2eedf9
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-14 21:00:03 -0300

    resolve ex056 do mundo 2

--- cursoemvideo/mundo-2/ex056.py
+++ cursoemvideo/mundo-2/ex056.py
@@ -2,3 +2,28 @@
 -A média de idade do grupo
 -Qual o nome do homem mais velho
 -Quantas mulheres têm menos de 20 anos'''
+somaIdade = 0
+mediaIdade = 0
+maioridadeHomem = 0
+nomeVelho = ''
+totalMulher20 = 0
+
+for i in range(1, 5):
+  print(f"-----------{i}ª PESSOA -----------")
+  nome = str(input("Nome ")).strip()
+  idade = int(input("Idade "))
+  sexo = str(input("Sexo [M/F]: "))
+  somaIdade += idade
+  if i == 1 and sexo in 'Mm':
+    maioridadeHomem = idade
+    nomeVelho = nome
+  if sexo in 'Mm' and idade > maioridadeHomem:
+    maioridadeHomem = idade 
+    nomeVelho = nome
+  if sexo in 'Ff' and idade < 20:
+    totalMulher20 += 1
+
+mediaIdade = somaIdade/4
+print(f"A média de idade do grupo de {mediaIdade} anos")
+print(f"O homem mais velho tem {maioridadeHomem} anos e se chama {nomeVelho}")
+print(f"Ao todo são {totalMulher20} mulheres com menos de 20 anos")
