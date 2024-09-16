2e83791ea791f68394e5aae4a1fb1dd64d5c99c3
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-15 21:23:04 -0300

    resolve ex061 do mundo 2

--- cursoemvideo/mundo-2/ex061.py
+++ cursoemvideo/mundo-2/ex061.py
@@ -1,2 +1,15 @@
-'''Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos de prgressão
+'''Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos de progressão
 usando a estrutura while.'''
+print("Gerador de PA")
+print("-="*15)
+
+primeiro = int(input("Primeiro termo: "))
+razao = int(input("Razão da PA: "))
+termo = primeiro
+contador = 1
+
+while contador <= 10:
+  print(f"{termo}"+"-> ", end='')
+  termo += razao
+  contador += 1
+print('FIM')
