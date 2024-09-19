fb8afe683e637d9632196e6f3c0e2e143c349cdc
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-18 23:52:25 -0300

    adiciona enunciado da questão 2 da prova 3

---
+++ algoritmos/provas/prova-03-2024/q2.py
@@ -0,0 +1,14 @@
+'''Você foi contratado pela Federação Paraibana de Futebol e recebeu uma tarefa, desenvolver um programa que receba
+como entrada uma sequência de resultados de partida de um determinado time e gere como sáida a pontuação do time de
+de acorod com os resultados. O resultado pode ser vitória, empate ou derrota. Uma vitória vale 3 pontos, empate 1 e
+derrota 0. Segue um exemplo de entrada. 
+3 2 
+0 0 
+1 1 
+1 4 
+6 2 
+1 0 
+2 1 
+1 0 
+A pontuação do time para esta sequência de jogos é 17, Observe que o placar sempre apresenta o número de gols do
+time na primeira posição.'''
