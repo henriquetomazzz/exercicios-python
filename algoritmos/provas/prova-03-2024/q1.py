8edc3447717915efecbf92be5e9bd0270c30cb02
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-19 21:50:22 -0300

    resolve q1 da prova 3

--- algoritmos/provas/prova-03-2024/q1.py
+++
@@ -1,3 +0,0 @@
-'''Considerando a expressão a seguir, qual seria o valor retornado, caso tivéssemos a = 4 b = 3 c = 1 e d = 2? 
-Deixe escrito na prova a resolução completa da expressão
-not(a>=C*d) or b-a < c and b**c+d == a'''
