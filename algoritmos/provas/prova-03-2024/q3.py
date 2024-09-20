8b74651901e7bcc39c00e8dcbaf982453216d544
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-19 22:50:43 -0300

    adiciona resto do enunciado da q3 da prova 3

--- algoritmos/provas/prova-03-2024/q3.py
+++ algoritmos/provas/prova-03-2024/q3.py
@@ -6,7 +6,15 @@ mensalmente uma assinatura de R$7, que inclui uma franquia de 10 m^3 de água. I
 e 10 m^3, o consumidor paga a mesma quantia de R$7 reais (note que o valor da assinatura deve ser pago mesmo que o 
 consumidor não tenha consumido água). Acima de 10 m^3, cada metro cúbico subsequente tem um valor diferente, 
 dependendo da faixa de consumo. A SBC cobra apenas por quantidades inteiras de metros cúbicos consumidos.
-A tabela abaixo especifica o preço por metro cúbico para cada faixa de consumo:'''
+A tabela abaixo especifica o preço por metro cúbico para cada faixa de consumo:
+<tabela>
+Assim, por exemplo, se o consumo foi de 120 m3, o valor da conta é:
+* 7 reais da assinatura básica;
+* 20 reais pelo consumo no intervalo 11 - 30 m3
+* 140 reais pelo consumo no intervalo 31 - 100 m3 ;
+* 100 reais pelo consumo no intervalo 101 - 120 m3
+Logo o valor total da conta de água é R$ 267. Escreva um programa que, dado o consumo de uma residência em m3 , 
+calcula o valor da conta de água daquela residência.'''
 consumo = float(input("Digite o consumo de água em m³: "))
 total = 7
 
