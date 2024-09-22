a79274ed71869e0e0f3069652b210d6f51b2b2e6
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-22 13:34:05 -0300

    resolve q2 da prova 3

--- algoritmos/provas/prova-03-2024/q2.py
+++ algoritmos/provas/prova-03-2024/q2.py
@@ -1,3 +1,13 @@
 '''Escreva um programa em Python que receba um inteiro N e em seguida leia N valores reais. O programa deve imprimir
 quantos dos N valores são positivos e a média destes (valores positivos). A média deve ser impressa com 1 casa 
 decimal.'''
+n = int(input())
+positivos = somador = 0
+
+for i in range(0, n+1):
+  entrada = float(input())
+  if entrada > 0:
+    positivos+=1
+    somador +=entrada
+
+print(f"{somador/positivos:.1f}")
