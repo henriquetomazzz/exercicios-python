f7760d24c347578b9c10e2dcc7a76a48f2e1ebed
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-22 13:28:16 -0300

    resolve q1 da prova 3

--- algoritmos/provas/prova-03-2024/q1.py
+++ algoritmos/provas/prova-03-2024/q1.py
@@ -1,2 +1,7 @@
 '''Escreva um programa em Python que receba os inteiros X e Y. Em seguida, imprima o quadrado de todos os números 
 ímpares no intervalo fechado de X a Y. '''
+x = int(input())
+y = int(input())
+
+for i in range(x, y+1):
+  print(i**2, end=" ")
