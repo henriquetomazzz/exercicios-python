5534d7c1f4af155d670d062a94cf6a6b918b9627
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-21 23:43:29 -0300

    resolve q3(a) da prova 4

--- algoritmos/provas/prova-04-2024/q3(a).py
+++ algoritmos/provas/prova-04-2024/q3(a).py
@@ -1,5 +1,21 @@
 '''Escreva uma função que receba um número inteiro, i, e retorne o i-ésimo elemento da série. Por exemplo, se o i 
 recebido for 6 o retorno deve ser 8 ou se o i recebido for 8 o retorno deve ser 21. Escreva uma função não recursiva. ''' 
+def fib(i): 
+  if i == 1 or i == 2:
+    return 1
+  
+  anterior = 1
+  corrente = 1
+
+  for j in range(i-2):
+    proximo = anterior+corrente
+    anterior = corrente 
+    corrente = proximo 
+
+  return corrente 
+
+i = int(input())
+print(fib(i))
 
 def palindromo(palavra):
   palavra = palavra.upper()
