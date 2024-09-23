f9fbfc5b4d927e813d776c29d0046c66063323f0
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-23 18:53:05 -0300

    resolve ex071 do mundo 2

--- cursoemvideo/mundo-2/ex071.py
+++ cursoemvideo/mundo-2/ex071.py
@@ -1,3 +1,28 @@
 '''Crie um programa que simule o funcionamento de um caixa eletrônico. No iício, pergunte ao usuário qual será o 
 valor a ser sacado(número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregue.
 OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1'''
+print("="*30)
+print("{:^30}".format("BANCO DEV"))
+print("="*30)
+valor = int(input("Que valor você quer sacar? R$"))
+total=valor
+ced = 50
+totCed = 0
+while True: 
+  if total >= ced:
+    total -= ced
+    totCed += 1
+  else: 
+    if totCed > 0: 
+      print(f"Total de {totCed} cédula de R${ced}")
+    if ced == 50:
+      ced = 20
+    elif ced == 20:
+      ced == 10
+    elif ced == 10:
+      ced = 1
+    totCed = 0
+    if total == 0:
+      break
+print("="*30)
+print("Volte sempre ao BANCO DEV! Tenha um bom dia!")
