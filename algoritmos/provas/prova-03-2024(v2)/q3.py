91a74b7d960baabe792f28d8c5679dc2c2387679
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-23 14:01:49 -0300

    adiciona enunciado da q3 da prova(v2) 3

---
+++ algoritmos/provas/prova-03-2024(v2)/q3.py
@@ -0,0 +1,13 @@
+'''Escreva um programa em Python que irá ler N números inteiros negativos e decrescentes inseridos pelo usuário. O 
+número deve ser sempre negativo e decrescente. Caso qualquer número inválido seja adicionado, deve ser descartado e 
+deve-se considerar um valor igual ao último número válido, menos um. O programa deve parar ao ler o número ZERO. No 
+final, deve ser exibido a soma da metade de todos os números como se todos fossem positivos.
+Ex. 1:
+ENTRADA: -8, -9, -10, -11, 2, 0
+CONSIDERA: -8, -9, -10, -11, -12
+SOMA DAS METADES (CONSIDERANDO TUDO POSITIVO): 25 → SAÍDA
+Ex. 2:
+ENTRADA: -8, -9, -10, -11, 2, -15, -15, 0
+CONSIDERA: -8, -9, -10, -11, -12, -15, -16
+SOMA DAS METADES (CONSIDERANDO TUDO POSITIVO): 40,5 → SAÍDA
+'''
