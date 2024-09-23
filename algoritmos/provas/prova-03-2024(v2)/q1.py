0f11bc50e87ea2cc67a54074d0a2e98babf2ee91
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-23 13:56:36 -0300

    adiciona enunciado da q1 da prova(v2) 3

---
+++ algoritmos/provas/prova-03-2024(v2)/q1.py
@@ -0,0 +1,12 @@
+'''Escreva um programa em Python que irá ler 3 números inteiros inseridos pelo usuário (X, Y, Z). Identifique qual é
+o menor número, o maior número e o número médio. Do menor número, subtraia o valor médio N vezes, onde N = valor 
+médio. Do maior número, adicione o valor médio N vezes, onde N = valor médio. Exiba todos os números múltiplos de 3 
+entre o menor valor encontrado e o maior valor encontrado, inclusive. Considere que sempre existirá um maior número, um menor número e um número médio, todos diferentes um do
+outro. (3,0)
+Ex:
+ENTRADA: 8, 3, 2
+MAIOR NÚMERO: 8 | MENOR NÚMERO: 2 | NÚMERO MÉDIO: 3
+1. ADICIONA O 3 AO NÚMERO 8 (3 VEZES) → 8 + 3 + 3 + 3 = 17 → NOVO MAIOR
+2. SUBTRAI O 3 DO NÚMERO 2 (3 VEZES) → 2 – 3 – 3 – 3 = -7 → NOVO MENOR
+3. INTERVALO [-7, 17]
+4. MÚLTIPLOS DE 3: -6, -3, 0, 3, 6, 9, 12, 15 → SAÍDA (LINHAS DIFERENTES)'''
