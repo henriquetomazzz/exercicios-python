efb6cca060c0b44389e1db7dc5af2b13a4d8e09f
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-23 18:46:28 -0300

    adiciona enunciado do ex071 do mundo 2

--- cursoemvideo/mundo-2/ex071.py
+++ cursoemvideo/mundo-2/ex071.py
@@ -1,1 +1,3 @@
-''''''
+'''Crie um programa que simule o funcionamento de um caixa eletrônico. No iício, pergunte ao usuário qual será o 
+valor a ser sacado(número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregue.
+OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1'''
