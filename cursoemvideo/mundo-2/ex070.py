39e811f401a8af6bae24818d03b7bf760c3c2bb3
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-23 18:41:57 -0300

    resolve ex070 do mundo 2

--- cursoemvideo/mundo-2/ex070.py
+++ cursoemvideo/mundo-2/ex070.py
@@ -3,3 +3,32 @@ continuar ou não. No final, mostre:
 a) qual é o total gasto na compra
 b) quantos produtos custam mais de R$1000
 c) qual é o nome do produto mais barato'''
+print("--"*15)
+print("\tLOJA SUPER BARATÃO")
+print("--"*15)
+
+total = totMil = menor = cont = 0
+barato = ''
+while True: 
+  nome = str(input("Nome do produto: "))
+  preco = float(input("Preço: R$"))
+  cont += 1 
+  total += preco
+  if preco > 100:
+    totMil += 1
+  if cont == 1 or preco < menor:
+    menor = preco
+    barato = nome
+
+  resp = ' '
+
+  while resp not in "SN":
+    resp = str(input("Quer continuar? [S/N]")).strip().upper()[0]
+
+  if resp in "N":
+    break
+
+print("--"*20 + " FIM DO PROGRAMA " + "--"*20)
+print(f"O preço total da compra {total:.2f}")
+print(f"Temos {totMil} produtos custando mais de R$1000.00")
+print(f"O produto mais barato foi {barato} que custa R${menor:.2f}")
