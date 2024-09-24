8a3f4224ba8db348b8e7cde8650fcb1ecdc7a263
henriquetomazzz <henriquetomaz021@gmail.com>
2024-09-24 14:05:33 -0300

    resolve ex086 do mundo 3

--- cursoemvideo/mundo-3/ex086.py
+++ cursoemvideo/mundo-3/ex086.py
@@ -1,12 +1,13 @@
-'''Crie um programa qye crie uma matriz de dimentsão 3x3 e preencha com valores lido pelo teclado. No final mostre
+'''Crie um programa que crie uma matriz de dimentsão 3x3 e preencha com valores lido pelo teclado. No final mostre
 a matriz na tela, com a formatação correta.'''
 matriz = [[0,0,0], [0,0,0], [0,0,0]]
 
-for linha in range(0, 3):
+for linha in range(0, 3): 
   for coluna in range(0, 3):
-    matriz[linha] [coluna] = int(input(f"Digite um valor para [{linha}][{coluna}]: "))
+    matriz[linha][coluna]=int(input(f"Digite um valor para [{linha}][{coluna}] "))
 
-for linha in range(0, 3):
-  for coluna in range(0, 3):
-    print(f"[{matriz[linha] [coluna]:^5}]", end='')
+print('-='*30)
+for linha in range(0,3): 
+  for coluna in range(0,3):
+    print(f"[{matriz[linha][coluna]:^5}]", end='')
   print()
