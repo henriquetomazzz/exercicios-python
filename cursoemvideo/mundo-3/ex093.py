0de37991e3b5911beb1530e80e18fbe5ff315f97
henriquetomazzz <henriquetomaz021@gmail.com>
2024-10-01 22:29:43 -0300

    resolve ex093 do mundo 3

--- cursoemvideo/mundo-3/ex093.py
+++ cursoemvideo/mundo-3/ex093.py
@@ -1,3 +1,12 @@
 '''Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e 
 quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será 
 guardado em um dicionário, incluindo o total de gols feitos durando o campeonato'''
+dados = dict()
+dados['nome'] = str(input('Nome do jogador: '))
+partidas = int(input(f'Quantas partidas {dados['nome']} jogou? '))
+
+for i in range(0, partidas+1): 
+  dados[f'partida {i}'] = int(input(f'Quantos gols na partida {i}'))
+
+print('-='*30)
+print(dados)
