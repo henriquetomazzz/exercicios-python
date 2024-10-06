ee87b958a895adc0115cb142bbdc681bb00bee1e
henriquetomazzz <henriquetomaz021@gmail.com>
2024-10-05 23:34:37 -0300

    corrige enunciado do ex106 do mundo 3

--- cursoemvideo/mundo-3/ex106.py
+++ cursoemvideo/mundo-3/ex106.py
@@ -1,3 +1,3 @@
-'''faça um programa que utilize o interativehelp do python. O usuário vai digitar o comando e o manual vai
+'''Faça um programa que utilize o interativehelp do python. O usuário vai digitar o comando e o manual vai
 aparecer. Quando o usuário digitar a palavra "FIM", o programa se encerrará
 obs: utilize cores'''
