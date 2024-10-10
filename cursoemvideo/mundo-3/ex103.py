f96c8f16fff48712197e6c9d45352b6d771ef1d3
henriquetomazzz <henriquetomaz021@gmail.com>
2024-10-09 22:17:43 -0300

    style: corrige enunciado do ex103 do mundo 3

--- cursoemvideo/mundo-3/ex103.py
+++ cursoemvideo/mundo-3/ex103.py
@@ -1,4 +1,4 @@
-'''faça um programa que tenha uma função chamado ficha(), que receba dois paramêtros opcionais: o nome de um
+'''Faça um programa que tenha uma função chamado ficha(), que receba dois paramêtros opcionais: o nome de um
 jogador e quantos gols ele marcou. O prorama deverá ser capaz de mostrar a ficha do jogador, mesmo que algum
 dado não tenha sido informado corretamente.'''
 def ficha(jogador = '<desconhecido>', gol = 0):
