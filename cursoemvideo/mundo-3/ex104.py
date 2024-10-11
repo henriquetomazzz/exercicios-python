bf0c974bf5d923f95a699b3ed5aaba1d6dc37082
henriquetomazzz <henriquetomaz021@gmail.com>
2024-10-10 22:16:49 -0300

    style: corrige enunciado da questão

--- cursoemvideo/mundo-3/ex104.py
+++ cursoemvideo/mundo-3/ex104.py
@@ -1,3 +1,3 @@
-'''crie um programa que tenha a função leiaint(), que vai funcionar de forma semelhante á função input()
+'''Crie um programa que tenha a função leiaint(), que vai funcionar de forma semelhante á função input()
 do python, só que fazendo a validação para aceitar apenas um valor númerico. 
 ex: n = leiaInt('Digite um n: ')'''
