89f9b18c327c14a383ac629ef4d03a7ee128850c
henriquetomazzz <henriquetomaz021@gmail.com>
2024-10-11 23:35:32 -0300

    style: corrige enunciado do ex006 do mundo 1

--- cursoemvideo/mundo-1/ex006.py
+++ cursoemvideo/mundo-1/ex006.py
@@ -1,4 +1,4 @@
-'''Cire um algoritmo que leia um número e mostre  o seu dobro, o seu triplo e a raiz quadrada'''
+'''Crie um algoritmo que leia um número e mostre o seu dobro, o seu triplo e a raiz quadrada'''
 import math
 num = int(input("Digite um número: "))
 print(f"O dobro desse valor é {num*2}, o triplo do seu valor é {num*3} e a raiz quadrada desse valor é {math.sqrt(num)}")
