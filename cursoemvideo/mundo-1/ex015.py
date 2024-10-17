3184904b3ce6a7f6a3422f9d2a5161d149460bc1
henriquetomazzz <henriquetomaz021@gmail.com>
2024-10-16 23:00:41 -0300

    chore: corrige enunciado do ex015 e ex017

--- cursoemvideo/mundo-1/ex015.py
+++ cursoemvideo/mundo-1/ex015.py
@@ -1,4 +1,5 @@
-'''Escreva um programa que pergunte a qunatidade de km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro  custa R$60 por dia e R$ 0,15 por km rodado '''
+'''Escreva um programa que pergunte a qunatidade de km percorridos por um carro alugado e a quantidade de dias pelos
+quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro  custa R$60 por dia e R$ 0,15 por km rodado '''
 km = float(input("Digite a quantidade de km percorridos: "))*0.15
 day = int(input("Digite a quantidade de dias que o carro foi alugado: "))*60
 
--- cursoemvideo/mundo-1/ex017.py
+++ cursoemvideo/mundo-1/ex017.py
@@ -1,4 +1,5 @@
-'''Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.'''
+'''Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule
+e mostre o comprimento da hipotenusa.'''
 import math
 co = float(input("Digite o valor do cateto oposto: "))
 ca = float(input("Digite o valor do cateto adjacente: "))
